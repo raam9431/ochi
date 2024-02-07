d6bfe7f1b9b1e208914b03af64f27688548381cf
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-08 02:53:34 +0530

    2.30am 8th feb sprint

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { IoArrowUpOutline } from "react-icons/io5";
+
 
 function LandingPage() {
   return (
@@ -14,9 +16,14 @@ function LandingPage() {
       <div className="border-t-[1px] border-zinc-800 flex mt-32 justify-between items-center py-5 px-20">
       {["For public and private companies", "From the first pitch to IPO"].map((item, index)=><p className='text-lg font-light tracking-tight leading-none'>{item}</p>)}
       <div className="start">
-        <div className="px-5 py-2 border-[2px] rounded-full border-zinc-500 font-light text-sm capitalize"></div>
-        <div className="w-3 h-3 rounded-full"></div>
-      </div>
+        <div className="px-5 py-2 border-[1px] rounded-full border-zinc-400 font-light text-lg uppercase">start the project
+        </div>
+          <div className="w-10 h-10 flex items-center justify-center rounded-full border-[2px] border-zinc-400">
+            <span className='rotate-[45deg]'>
+               <IoArrowUpOutline />
+            </span>  
+          </div>
+        </div>
       </div>
     </div>
   )
