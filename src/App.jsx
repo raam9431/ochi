78b665c28693868f70f99ea9f94ea3d9a819b336
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-07 22:33:01 +0530

    made some corrections

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>    
+       
     <div className='w-full h-screen bg-zinc-900 text-white'>App
     <Navbar />
     <LandingPage />
     </div>
-    </>
+    
   )
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function LandingPage() {
   return (
     <div className='w-full h-screen bg-zinc-900 pt-1'>
-      <div className='textstructure mt-40 px-20'>
-        <div className='masker '>
-          <h1 className="uppercase text-9xl leading-none tracking-tighter font-['Founders_Grotesk' font-semibold]">We Create</h1>
+      <div className='textstructure mt-52 px-20'>
+        <div className='masker font-light'>
+        <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter font-['Founders_Grotesk'] font-medium" >We Create</h1>
         </div>
       </div>
     </div>
