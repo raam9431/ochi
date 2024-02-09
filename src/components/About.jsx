45e6ab44dcda0b79e7d82ef73cd5def6aea92c5a
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-10 01:04:13 +0530

    start about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 function About() {
     return (
         <div className='w-full p-20 bg-[#CDEA68] rounded-tr-3xl rounded-tl-3xl text-black '>
-            <h1 className='font-["Neue_Montreal"] text-6xl'>
+            <h1 className='font-["Neue_Montreal"] text-[4vw] leading-[3.1vw] tracking-tighter'>
                 Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.
             </h1>
+
+            <div className='w-full border-t-[1px] pt-10 mt-20 border-[#364308]'>
+                <div className='w-1/2'>
+                <h1 className='text-7xl'>Our Approach:</h1>
+                <button className='flex uppercase gap-10 items-center px-10 py-3 mt-10 bg-zinc-900 rounded-full text-white'>Read More
+                <div className='w-2 h-2 bg-zinc-100 rounded-full'></div>
+                </button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,10 +8,10 @@ function LandingPage() {
       <div className='textstructure mt-40 px-20'>
         {["We Create", "Eye Opening", "Presentation"].map((item, index) => {
           return (
-            <div className="masker">
+            <div className="masker bg-red-950">
               <div className='w-fit flex items-end'>
                 {index === 1 && (
-                  <div className='mr-5 w-[8vw] rounded-md h-[5.7vw] -top-[1.2vw] relative bg-red-500'></div>
+                  <div className='mr-5 mt-6 w-[8vw] rounded-md h-[5.7vw] -top-[1.2vw] relative bg-green-500'></div>
                 )}
                 <h1 className="pt-[2vw] -mb-[1vw] uppercase text-[9vw] leading-[.75] font-['Founders_Grotesk_X-Condensed'] font-bold" >
                   {item}
