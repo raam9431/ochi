2e85a23512f4abab0b8f9e5f7e471d59276cfc99
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-10 03:29:08 +0530

    start featured.jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -7,14 +7,14 @@ function Featured() {
                 <h1 className='text-7xl font-["Neue_Montreal"] tracking-tighter'>Featured Projects</h1>
                 <div className='px-20'>
                     <div className="cards w-full flex gap-10 mt-10">
-                        <div className="cardcontainer w-1/2 h-[75vh] bg-red-500 overflow-hidden scale-50">
-                            <div className="card rounded-xl w-full h-full bg-green-700">
-                                <img className="w-full h-full bg-cover" src="" alt="" />
+                        <div className="cardcontainer w-1/2 h-[75vh] ">
+                            <div className="card rounded-xl w-full h-full overflow-hidden">
+                                <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
                             </div>
                         </div>
-                        <div className="cardcontainer w-1/2 h-[75vh] bg-red-500 overflow-hidden scale-50">
-                            <div className="card rounded-xl w-full h-full bg-green-700">
-                                <img className="w-full h-full bg-cover" src="" alt="" />
+                        <div className="cardcontainer w-1/2 h-[75vh] ">
+                            <div className="card rounded-xl w-full h-full overflow-hidden">
+                                <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="" />
                             </div>
                         </div>
                     </div>
