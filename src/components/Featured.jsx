bcfc528aaa72638b0023719b5041af628488d328
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-11 11:41:06 +0530

    continue .jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -8,13 +8,17 @@ function Featured() {
                 <div className='px-20'>
                     <div className="cards w-full flex gap-10 mt-10">
                         <div className="cardcontainer relative w-1/2 h-[75vh] ">
-                        
-                        <h1 className='absolute pb-20 left-full text-[#CDEA68] -translate-x-1/2 -translate-y-1/2 z-9 leading-none tracking-tighter text-8xl'>FYDE</h1>
-                            <div className="card rounded-xl w-full h-full overflow-hidden">
+                            <h1 className='absolute pb-20 right-full z-[9] text-[#e25e44] translate-x-1/2 top-1/2 -translate-y-1/2 z-9 leading-none tracking-tighter text-8xl'>
+                                {"FYDE".split('').map((item, index) => <span className=''>{item}</span>)}
+                            </h1>                            <div className="card rounded-xl w-full h-full overflow-hidden">
                                 <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
                             </div>
                         </div>
-                        <div className="cardcontainer w-1/2 h-[75vh] ">
+                        <div className="bg-zinc-100 relative cardcontainer w-1/2 h-[75vh] ">
+                            <h1 className='absolute pb-20 right-full z-[9] text-[#e25e44] translate-x-1/2 top-1/2 -translate-y-1/2 z-9 leading-none tracking-tighter text-8xl'>
+                                {"VISE".split('').map((item, index) => <span className=''>{item}</span>)}
+                            </h1>
+
                             <div className="card rounded-xl w-full h-full overflow-hidden">
                                 <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="" />
                             </div>
