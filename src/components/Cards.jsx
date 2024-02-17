ba1d1618c1d9912e1a2d3e516c7b08b043f799fe
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-18 00:20:02 +0530

    Cards.jsx sprint

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,14 +6,14 @@ function Cards() {
             <div className="cardcontainer h-[50vh] w-1/2 ">
                 <div className="card w-full h-full bg-[#004D43] rounded-xl flex items-center justify-center relative">
                     <img className="justify-items-center" src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 '>&copy;</button>
+                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 border-[#CDEA68] text-[#CDEA68]'>&copy;2019-2025</button>
                 </div>
             </div>
             <div className="cardcontainer flex gap-10 h-[50vh] w-1/2">
-                <div className="card w-1/2 h-full bg-[#212121] rounded-tl-xl">
+                <div className="card w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center">
                     <img src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
                 </div>
-                <div className="card w-1/2 h-full bg-[#212121] rounded-tr-xl">
+                <div className="card w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center">
                     <img src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
                 </div>
             </div>
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -10,7 +10,8 @@ function Featured() {
                         <div className="cardcontainer relative w-1/2 h-[75vh] ">
                             <h1 className='absolute pb-20 left-full z-[9] text-[#CDEA68] -translate-x-1/2 top-1/2 -translate-y-1/2 z-9 leading-none tracking-tighter text-8xl'>
                                 {"FYDE".split('').map((item, index) => <span className=''>{item}</span>)}
-                            </h1>                            <div className="card rounded-xl w-full h-full overflow-hidden">
+                            </h1>
+                            <div className="card rounded-xl w-full h-full overflow-hidden">
                                 <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
                             </div>
                         </div>
