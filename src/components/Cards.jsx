158d8acd4f7b99710422b86b4f2d73850e74f42a
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-20 00:52:16 +0530

    Cards.jsx sprint

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,16 +12,16 @@ function Cards() {
             <div className=" cardcontainer flex gap-5 h-[60vh] w-1/2">
                 <div className="card relative w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center">
                     <img src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
-                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 border-[#F1F1F1] text-[#F1F1F1] hover:bg-[#F1F1F1] hover:text-[#212121] hover:font-semibold '>
+                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 border-[#F1F1F1] text-[#F1F1F1] hover:bg-[#F1F1F1] hover:text-[#212121] hover:font-semibold font-semibold'>
                         Rating 5.0 on Clutch
                     </button>
                 </div>
                 <div className="card relative w-1/2 h-full bg-[#212121] rounded-xl flex items-center justify-center">
                     <img src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt=" " />
-                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 border-[#F1F1F1] text-[#F1F1F1] hover:bg-[#F1F1F1] hover:text-[#212121] hover:font-semibold'>
+                    <button className='absolute left-10 bottom-10 px-5 py-1 rounded-full border-2 border-[#F1F1F1] text-[#F1F1F1] hover:bg-[#F1F1F1] hover:text-[#212121] hover:font-semibold font-semibold'>
                         Business Bootcamp Alumni
-                    </button>                    
-                </div>
+                    </button>
+                </div> 
             </div>
         </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='w-full h-screen bg-zinc-900'>
             <div className='w-1/2 font-["Founders_Grotesk_X-Condensed"] p-20'>
-                <h1>Eye </h1>
+                <h1 className='bold font-[10px]'>Eye </h1>
                 <h1>Opening</h1>
             </div>
             <div className='w-1/2 font-["Founders_Grotesk_X-Condensed"]'>
