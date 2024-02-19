4a11e570ee41ea5e22aa746558b91f37d538f275
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-02-20 00:12:31 +0530

    Cards.jsx sprint

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About'
 import Cards from './components/Cards'
 import Eyes from './components/Eyes'
 import Featured from './components/Featured'
+import Footer from './components/Footer'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import Navbar from './components/Navbar'
@@ -18,6 +19,7 @@ function App() {
       <Eyes />
       <Featured />
       <Cards />
+      <Footer />
     </div>
 
   )
